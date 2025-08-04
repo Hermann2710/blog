a4ec14ce684051d678eea30b3adbf24f724804f4
Hermann2710 <138356589+Hermann2710@users.noreply.github.com>
2025-08-04 14:02:34 +0100

    6- Utilitaire pour gérer le token

---
+++ backend/src/models/user.ts
@@ -0,0 +1,28 @@
+import { Document, model, Schema } from "mongoose"
+
+export interface IUser extends Document {
+  name: string
+  username: string
+  email: string
+  password: string
+  role: "admin" | "author" | "reader"
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    name: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["admin", "author", "reader"],
+      default: "reader",
+    },
+  },
+  { timestamps: true }
+)
+
+const User = model<IUser>("User", userSchema)
+
+export default User
