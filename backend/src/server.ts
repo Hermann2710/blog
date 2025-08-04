04e0bea4ff67b6cf042cfab64e16946cc1a19d00
Hermann2710 <138356589+Hermann2710@users.noreply.github.com>
2025-08-04 20:23:27 +0100

    16 - Mise à jour de server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,9 @@
 import express from "express"
-import dotenv from "dotenv"
 import cors from "cors"
 import connectDB from "./config/db"
 import authRoutes from "./routes/authRoute"
 import { appConfig } from "./config"
+import categoryRoutes from "./routes/categoryRoute"
 
 connectDB()
 
@@ -13,6 +13,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use("/api/auth", authRoutes)
+app.use("/api/categories", categoryRoutes)
 
 app.listen(appConfig.port, () =>
   console.log(`Serveur démarré sur le port ${appConfig.port}`)
