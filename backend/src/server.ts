20653ce80adc88e503107aa29b07d4160fd8c23a
Hermann2710 <138356589+Hermann2710@users.noreply.github.com>
2025-08-04 15:18:13 +0100

    11 - Fichier server.ts

---
+++ backend/src/server.ts
@@ -0,0 +1,20 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import connectDB from "./config/db"
+import authRoutes from "./routes/authRoute"
+import { appConfig } from "./config"
+
+dotenv.config()
+connectDB()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/auth", authRoutes)
+
+app.listen(appConfig.port, () =>
+  console.log(`Serveur démarré sur le port ${appConfig.port}`)
+)
