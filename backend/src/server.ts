2993a6d978aae5a762e66bd685120e647628e14e
Hermann2710 <138356589+Hermann2710@users.noreply.github.com>
2025-08-04 21:24:22 +0100

    22 - Mise à jour du server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,8 @@ import connectDB from "./config/db"
 import authRoutes from "./routes/authRoute"
 import { appConfig } from "./config"
 import categoryRoutes from "./routes/categoryRoute"
+import articleRoutes from "./routes/articleRoute"
+import path from "path"
 
 connectDB()
 
@@ -11,9 +13,11 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")))
 
 app.use("/api/auth", authRoutes)
 app.use("/api/categories", categoryRoutes)
+app.use("/api/articles", articleRoutes)
 
 app.listen(appConfig.port, () =>
   console.log(`Serveur démarré sur le port ${appConfig.port}`)
