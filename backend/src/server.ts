5f52528ede026fd4b6de828d1dd03e9b97a9dfae
Hermann2710 <138356589+Hermann2710@users.noreply.github.com>
2025-08-04 22:43:18 +0100

    28 - Intégration dans server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,6 +7,7 @@ import categoryRoutes from "./routes/categoryRoute"
 import articleRoutes from "./routes/articleRoute"
 import path from "path"
 import userRoutes from "./routes/userRoutes"
+import commentRoutes from "./routes/commentRoutes"
 
 connectDB()
 
@@ -20,6 +21,7 @@ app.use("/api/auth", authRoutes)
 app.use("/api/user", userRoutes)
 app.use("/api/categories", categoryRoutes)
 app.use("/api/articles", articleRoutes)
+app.use("/api/comments", commentRoutes)
 
 app.listen(appConfig.port, () =>
   console.log(`Serveur démarré sur le port ${appConfig.port}`)
